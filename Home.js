68ce7d2f9280e908af2d68a394c94ee85f688881
KaanaxD <lmarkabmaulanam348@gmail.com>
2025-04-04 20:45:54 +0800

    Mufrodat 2.0

--- Home.js
+++ Home.js
@@ -1,43 +1,17 @@
-var tombol1 = document.getElementById("tombol1");
-var tombol2 = document.getElementById("tombol2");
-var a2 = document.getElementById("a2");
-var a1 = document.getElementById("a1");
+const Mufrodat= document.getElementById("NavMufrodat");
+const Vocabulary= document.getElementById("NavVocabulary");
+const Default= document.getElementById("default");
+const NavvMufrodat= document.getElementById("btn1");
+const NavvVocabulary= document.getElementById("btn2");
 
-const ya = document.getElementById ("ya");
-const tidak = document.getElementById ("tidak");
-const biasa = document.getElementById ("biasa");
-let subbmit = document.getElementById ("subbmit");
-const jawaban = document.getElementById ("jawaban");
-let feedback = document.getElementById ("feedback");
-
-tombol1.onclick = function(){
-        a1.style.display = "flex";
-        a2.style.display = "none";
-        tombol2.style.backgroundColor = "aliceblue";
-        tombol2.style.color = "black";    
-        tombol1.style.backgroundColor = "black";
-        tombol1.style.color = "aliceblue";
-        feedback.style.display = "flex";
+NavvMufrodat.onclick = function(){
+    Mufrodat.style.display= "flex";
+    Vocabulary.style.display= "none";
+    Default.style.display= "none";
 }
 
-tombol2.onclick = function(){
-    a1.style.display = "none";
-    a2.style.display = "flex";
-    tombol1.style.backgroundColor = "aliceblue";
-    tombol1.style.color = "black";
-    tombol2.style.backgroundColor = "black";
-    tombol2.style.color = "aliceblue";
-    feedback.style.display = "flex";
-}
-
-subbmit.onclick = function(){
-    if (ya.checked){
-        jawaban.textContent = "thnks pendapatnya";
-    } else if (tidak.checked) {
-        jawaban.textContent = "serah lu deh";
-    } else if (biasa.checked) {
-        jawaban.textContent = "kecewa";
-    } else {
-        jawaban.textContent = "Pilih salah satu!";
-    }
+NavvVocabulary.onclick = function(){
+    Mufrodat.style.display= "none";
+    Vocabulary.style.display= "flex";
+    Default.style.display= "none";
 }
