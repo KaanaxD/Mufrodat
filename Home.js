081c8f84e97063b9fd0fc5f1800dcde4cbfdec7d
KaanaxD <lmarkabmaulanam348@gmail.com>
2025-04-05 15:33:36 +0800

    Update

--- Home.js
+++ Home.js
@@ -1,24 +1,41 @@
 const Mufrodat= document.getElementById("NavMufrodat");
 const Vocabulary= document.getElementById("NavVocabulary");
 const Default= document.getElementById("default");
-const NavvMufrodat= document.getElementById("btn1");
-const NavvVocabulary= document.getElementById("btn2");
 const Mpage1= document.getElementById("Mpage1");
 const Mpage2= document.getElementById("Mpage2");
 const Vpage1= document.getElementById("Vpage1");
 const Vpage2= document.getElementById("Vpage2");
+const navM= document.getElementById("Mmenu");
+const navV= document.getElementById("Vmenu");
 
 
-NavvMufrodat.onclick = function(){
+function awal(){
+    Vpage2.style.display="none";
+    Mpage2.style.display="none";
+}
+
+function MainMufrodat(){
     Mufrodat.style.display= "flex";
     Vocabulary.style.display= "none";
     Default.style.display= "none";
+    Mpage1.style.display="flex";
+    navM.style.backgroundColor="#393646";
+    navM.style.color="#F4EEE0";
+    navV.style.backgroundColor="#d3d3d3";
+    navV.style.color="#393646";
+    awal();
 }
 
-NavvVocabulary.onclick = function(){
-    Mufrodat.style.display= "none";
+function MainVocab(){
+    Mufrodat.style.display= "none"; 
     Vocabulary.style.display= "flex";
-    Default.style.display= "none";M
+    Default.style.display= "none";
+    Vpage1.style.display="flex";
+    navV.style.backgroundColor="#393646";
+    navV.style.color="#F4EEE0";
+    navM.style.backgroundColor="#d3d3d3";
+    navM.style.color="#393646";
+    awal();
 }
 
 function Mpagee1(){
