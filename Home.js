ab6ae2534b42b79e5d19a400c95a6bde67ef26d9
KaanaxD <lmarkabmaulanam348@gmail.com>
2025-04-05 09:19:45 +0800

    Update Home.js

--- Home.js
+++ Home.js
@@ -3,6 +3,11 @@ const Vocabulary= document.getElementById("NavVocabulary");
 const Default= document.getElementById("default");
 const NavvMufrodat= document.getElementById("btn1");
 const NavvVocabulary= document.getElementById("btn2");
+const Mpage1= document.getElementById("Mpage1");
+const Mpage2= document.getElementById("Mpage2");
+const Vpage1= document.getElementById("Vpage1");
+const Vpage2= document.getElementById("Vpage2");
+
 
 NavvMufrodat.onclick = function(){
     Mufrodat.style.display= "flex";
@@ -13,5 +18,25 @@ NavvMufrodat.onclick = function(){
 NavvVocabulary.onclick = function(){
     Mufrodat.style.display= "none";
     Vocabulary.style.display= "flex";
-    Default.style.display= "none";
+    Default.style.display= "none";M
+}
+
+function Mpagee1(){
+    Mpage2.style.display="none";
+    Mpage1.style.display="flex";
+}
+
+function Mpagee2(){
+    Mpage2.style.display="flex";
+    Mpage1.style.display="none";
+}
+
+function Vpagee1(){
+    Vpage1.style.display="flex";
+    Vpage2.style.display="none";
+}
+
+function Vpagee2(){
+    Vpage2.style.display="flex";
+    Vpage1.style.display="none";
 }
