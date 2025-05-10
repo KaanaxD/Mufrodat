9674d2af8432ce0c74d159caea5ec35d3b746855
KaanaxD <lmarkabmaulanam348@gmail.com>
2025-05-10 08:54:23 +0800

    up countdown

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ const minute = document.getElementById("minute");
 const second = document.getElementById("second");
 
 // Tentukan tanggal tujuan
-const targetDate = new Date("May 9, 2025 20:00:00").getTime();
+const targetDate = new Date("May 23, 2025 20:00:00").getTime();
 
 // Update countdown setiap detik
 const interval = setInterval(() => {
